66c34aee13aad71e8fbf1b1463cf5c2ebe73f11b
arjay07 <arjay.07.ly@gmail.com>
2022-08-23 16:37:03 -0600

    CLUCK-2 Add landing module.

---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingScreenComponent } from './screens/landing-screen/landing-screen.component';
+
+
+
+@NgModule({
+  declarations: [
+    LandingScreenComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingModule { }
