4a8c5964dd47bd144a84ff979a7da7acf89158f8
arjay07 <arjay.07.ly@gmail.com>
2022-10-19 15:29:04 -0600

    CLUCK-23 Add user service to handle following and followers.

---
+++ src/app/clucker/services/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserUpdateRequest} from '@models/user-update-request';
+import {environment} from '@env';
+import {Observable} from 'rxjs';
+import {Page} from '@models/page';
+import {User} from '@models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  api = environment.api;
+
+  constructor(private http: HttpClient) { }
+
+  updateUserProfile(id: number, userUpdate: UserUpdateRequest) {
+    return this.http.put(`${this.api}/users/${id}`, userUpdate);
+  }
+
+  followUser(id: number) {
+    return this.http.put(`${this.api}/users/${id}/followers`, null);
+  }
+
+  unfollowUser(id: number) {
+    return this.http.delete(`${this.api}/users/${id}/followers`);
+  }
+
+  getFollowers(id: number): Observable<Page<User>> {
+    return this.http.get<Page<User>>(`${this.api}/users/${id}/followers`);
+  }
+
+  getFollowing(id: number): Observable<Page<User>> {
+    return this.http.get<Page<User>>(`${this.api}/users/${id}/following`);
+  }
+
+}
