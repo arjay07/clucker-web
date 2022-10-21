78898dc70e80e2b50e1b4ffd65e345d962e5797e
arjay07 <arjay.07.ly@gmail.com>
2022-10-20 21:01:36 -0600

    CLUCK-27 Add users cache user.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {map, Observable, of} from 'rxjs';
 import {User} from '@models/user';
 import {environment} from '@env';
 import {UserUpdateRequest} from '@models/user-update-request';
 import {Page} from '@models/page';
+import {Cache} from '@interfaces/cache';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,31 @@ export class UserService {
 
   api = environment.api;
 
+  userIdCache: Cache<number, User> = new Cache<number, User>();
+  usernameCache: Cache<string, User> = new Cache<string, User>();
+
   constructor(private http: HttpClient) { }
 
   getUserById(id: number): Observable<User> {
-    return this.http.get<User>(`${this.api}/users/${id}`);
+    const cachedUser = this.userIdCache.getById(id);
+    if (cachedUser)
+      return cachedUser;
+    return this.http.get<User>(`${this.api}/users/${id}`)
+      .pipe(map(value => {
+        this.userIdCache.addToCache(value.id, of(value));
+        return value;
+      }));
   }
 
   getUserByUsername(username: string) {
-    return this.http.get<User>(`${this.api}/users/username/${username}`);
+    const cachedUser = this.usernameCache.getById(username);
+    if (cachedUser)
+      return cachedUser;
+    return this.http.get<User>(`${this.api}/users/username/${username}`)
+      .pipe(map(value => {
+        this.usernameCache.addToCache(value.username, of(value));
+        return value;
+      }));
   }
 
   getSelf(): Observable<User> {
